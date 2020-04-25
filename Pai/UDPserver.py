d5fb7490234e03af47ce61ede3d9a0cc95b8b5ab
Rose666666 <993006297@qq.com>
2020-04-26 00:28:50 +0800

    可以用

--- Pai/UDPserver.py
+++ Pai/UDPserver.py
@@ -1,5 +1,5 @@
 import socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(('192.168.0.100',1901))
+sock.bind(('主机IP','端口'))
 data, addr = sock.recvfrom(1024)
 print(data.decode())
