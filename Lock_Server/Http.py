686d2a3b034499344506fc189fe1d95831306cc8
Rose666666 <993006297@qq.com>
2020-04-28 17:07:59 +0800

    添加了HTTP模块

--- Lock_Server/Http.py
+++ Lock_Server/Http.py
@@ -1,7 +1,18 @@
 # 树莓派的HTTP模块 只是测试通过 逻辑还没怎么写（预警信息）
 import requests
+from datetime import datetime
+data = {
+    "name":'陌生人',
+    "event":'攻击',
+    "occur_time":datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+    "app_username":'lsd',
+    "lock_id":'2020',
+    "isSafe":'True'
+}
 
-response = requests.get("http://127.0.0.1:5000/app/login")
+
+response = requests.post("http://127.0.0.1:5000/app/WarningInfo", data)
+
+print(response)
+print(response.content.decode())
 print(response.headers)
-print(response.request)
-print(response.json()['isTrue'])
